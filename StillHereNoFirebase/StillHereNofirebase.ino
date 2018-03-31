533134d87f879093654b0a3098eaaac4b1acd8ef
Pok Supakit <mr.pokiloli@gmail.com>
2018-03-31 21:56:38 +0700

    Config Firebase

--- StillHereNoFirebase/StillHereNofirebase.ino
+++ StillHereNoFirebase/StillHereNofirebase.ino
@@ -1,11 +1,17 @@
 #include <Ultrasonic.h>
+#include <FirebaseArduino.h>
+
+
 Ultrasonic ultrasonic1(2, 3); //back 2 is trigle and 3 is echo
 Ultrasonic ultrasonic2(4, 5); //top 4 is trigle and 5 is echo
 double distance1, distance2; //distance1 form ultrasonic1  && distance2 form ultrasonic2
+#define FIREBASE_HOST "comproiot.firebaseio.com"
+#define FIREBASE_AUTH "UktGd4fGZ6JSA3chsZdrFGKcq5ObLCaomdXXd7eB"
 
 void setup() {
   Serial.begin(9600);
   pinMode(6, OUTPUT); // output buzzer
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 void loop() {
