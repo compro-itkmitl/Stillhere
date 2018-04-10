486a4ca7c679add694d5f712636ad849ea460572
waiwarit <60070087@kmitl.ac.th>
2018-04-11 00:44:17 +0700

    AddFuncBeep

--- StillHereNoFirebase/StillHereNofirebase.ino
+++ StillHereNoFirebase/StillHereNofirebase.ino
@@ -1,28 +1,43 @@
 #include <Ultrasonic.h>
-#include <FirebaseArduino.h>
-
 
 Ultrasonic ultrasonic1(2, 3); //back 2 is trigle and 3 is echo
 Ultrasonic ultrasonic2(4, 5); //top 4 is trigle and 5 is echo
 double distance1, distance2; //distance1 form ultrasonic1  && distance2 form ultrasonic2
-#define FIREBASE_HOST "comproiot.firebaseio.com"
-#define FIREBASE_AUTH "UktGd4fGZ6JSA3chsZdrFGKcq5ObLCaomdXXd7eB"
+double beepCount;
+
 
 void setup() {
   Serial.begin(9600);
   pinMode(6, OUTPUT); // output buzzer
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 void loop() {
   distance1 = ultrasonic1.distanceRead(); //distanceRead is return cm
   distance2 = ultrasonic2.distanceRead(); //distanceRead is return cm
-  if (distance1 > 7 && distance2 > 7 ){
+  /*if (distance1 > 7 && distance2 > 7 ) {
     Serial.print("Yes");
     Serial.println();
   }
-  else{
+  else {
     Serial.println("No");
+  }*/
+  Serial.println(distance1);
+  if (distance1 < 5 ){
+    beep(distance1);
   }
-  digitalWrite(6, HIGH); 
+  
+  else{
+  digitalWrite(6, HIGH);
+  }
+}
+
+void beep(int distance) {
+ 
+  beepCount += 5; //delay sound
+  if (beepCount / (distance * 100) > 1) { //distance * n if n lower sound will many beep
+    digitalWrite(6, LOW);
+    beepCount = 0;
+  }else if(beepCount > 100){
+    digitalWrite(6, HIGH);
+  }
 }
