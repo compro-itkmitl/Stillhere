ae58dda2c5d6b795b56097a271a2487c99d7c527
WaiWarit <33046422+WaiWarit@users.noreply.github.com>
2018-04-12 14:09:10 +0700

    Connect Wifi And Adruino

--- Node_Stillhere/Node_Stillhere.ino
+++ Node_Stillhere/Node_Stillhere.ino
@@ -1,23 +1,34 @@
 #include <ESP8266WiFi.h>
-const char* ssid = "Nonthakorn";
-const char* password = "nootwainon"; 
+#include <SoftwareSerial.h>
+const char* ssid = "Nontakorn"; //ชื่อ WIFI
+const char* password = "nootwainon"; // Password ของ WIFI
+SoftwareSerial NodeSerial(D2, D3); //RX | TX
 void setup()
 {
   Serial.begin(115200);
   delay(10);
-  Serial.print("Connecting to ");
-  Serial.println(ssid); 
+  Serial.print("Connecting to "); 
+  Serial.println(ssid); //แสดงข้อความ ชื่อ WIFI
  
-  while (WiFi.status() != WL_CONNECTED) //รอจนกว่าจะเชื่อมต่อสำเร็จ
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print("*");
   }
   Serial.println("");
-  Serial.println("WiFi connected"); //แสดงข้อความเชื่อมต่อสำเร็จ
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP()); //แสดงหมายเลข IP NodeMCU ของเรา
-}
-void loop() {
+  Serial.println(WiFi.localIP());
 
+  pinMode(D2, INPUT);
+  pinMode(D3, OUTPUT);
+  NodeSerial.begin(4800);
+}
+
+void loop() {
+  NodeSerial.print(10);
+  NodeSerial.print("\n");
+  delay(100);
 }
