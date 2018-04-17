774dd692ac5d47a388cf5813e52603abff35a221
yves99 <60070104@kmitl.ac.th>
2018-04-18 03:01:10 +0700

    fix code

--- Node_Stillhere/Node_Stillhere.ino
+++ Node_Stillhere/Node_Stillhere.ino
@@ -1,8 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
 #include <FirebaseArduino.h>
-const char* ssid = "leelapvw"; //ชื่อ WIFI
-const char* password = "jh4331kp"; // Password ของ WIFI
+
+#define ssid "leelapvw" //ชื่อ WIFI
+#define password "jh4331kp" // Password ของ WIFI
 SoftwareSerial NodeSerial(D2, D3); //RX | TX
 #define FIREBASE_HOST "comproiot.firebaseio.com"
 #define FIREBASE_AUTH "UktGd4fGZ6JSA3chsZdrFGKcq5ObLCaomdXXd7eB"
@@ -11,25 +12,10 @@ SoftwareSerial NodeSerial(D2, D3); //RX | TX
 void setup()
 {
   Serial.begin(115200);
-  Serial.print("Connecting to "); 
-  Serial.println(ssid); //แสดงข้อความ ชื่อ WIFI
- 
   WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print("*");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
   pinMode(D2, INPUT);
   pinMode(D3, OUTPUT);
   NodeSerial.begin(4800);
-
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
